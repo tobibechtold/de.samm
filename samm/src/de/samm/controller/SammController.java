a7fe52c00746d6cd0109fbb30775b76239b899dd
Tobias Bechtold <tobi.bechtold@gmail.com>
2013-10-07 13:53:28 +0200

    LoginView created

--- samm/src/de/samm/controller/SammController.java
+++ samm/src/de/samm/controller/SammController.java
@@ -1,10 +1,12 @@
 package de.samm.controller;
 
+import de.samm.view.SammLoginView;
 import de.samm.view.SammView;
 
 public class SammController {
 	
 	public static void main(String[] args0) {
+		new SammLoginView();
 		new SammView();
 		System.out.println("launched");
 	}
---
+++ samm/src/de/samm/view/SammLoginView.java
@@ -0,0 +1,102 @@
+package de.samm.view;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+
+public class SammLoginView extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5634744825871179717L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Create the frame.
+	 */
+	public SammLoginView() {
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 318, 176);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblPasswort = new JLabel("Passwort:");
+		
+		JButton btnRegistrieren = new JButton("Registrieren");
+		
+		JButton btnLogin = new JButton("Login");
+		
+		JButton btnAbbrechen = new JButton("Abbrechen");
+		
+		passwordField = new JPasswordField();
+		setVisible(true);
+		JCheckBox chckbxLoginMerken = new JCheckBox("Login merken");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblEmail)
+								.addComponent(lblPasswort))
+							.addGap(24)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(passwordField)
+								.addComponent(textField, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+								.addComponent(chckbxLoginMerken))
+							.addContainerGap(20, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(btnLogin, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addPreferredGap(ComponentPlacement.RELATED))
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(btnRegistrieren, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(btnAbbrechen, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE))
+							.addGap(20))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEmail)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblPasswort)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxLoginMerken)
+					.addGap(13)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnLogin, Alignment.LEADING)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+							.addComponent(btnAbbrechen)
+							.addComponent(btnRegistrieren)))
+					.addGap(6))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
