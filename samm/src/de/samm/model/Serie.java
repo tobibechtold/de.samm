14a3f91e2fc1554f0c08009f75b14024903b4159
Tobias Bechtold <tobi.bechtold@gmail.com>
2013-10-18 15:16:07 +0200

    Eine Serie hat nun auch einen Sender

--- samm/src/de/samm/model/Serie.java
+++ samm/src/de/samm/model/Serie.java
@@ -7,12 +7,13 @@ import java.util.List;
 public class Serie extends AbstractMedium {
 
 	private List<Staffel> staffeln;
+	private String sender;
 	
 	public Serie(String titel, String regisseur, List<String> schauspieler,
 			double wertung, String plot, Calendar release, File cover,
-			String genre) {
+			String genre, String sender) {
 		super(titel, regisseur, schauspieler, wertung, plot, release, cover, genre);
-		// TODO Auto-generated constructor stub
+		this.sender = sender;
 	}
 	
 	public void addStaffel(Staffel s) {
@@ -26,5 +27,23 @@ public class Serie extends AbstractMedium {
 	public List<Staffel> getStaffelListe() {
 		return staffeln;
 	}
+
+	public List<Staffel> getStaffeln() {
+		return staffeln;
+	}
+
+	public void setStaffeln(List<Staffel> staffeln) {
+		this.staffeln = staffeln;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	
+	
 	
 }
